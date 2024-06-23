9ebaab20bc12b4adfde1cf48323c2162947a49bd
Yathisha15 <yathisha.15177@gmail.com>
2024-06-23 11:24:02 +0530

    some minor changes

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 const Listing = require("./models/listing.js");
 const Review = require("./models/review.js");
-const ExpressError = require("./utils/expressError.js");
+const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema,reviewSchema } = require("./schema.js");
 
 module.exports.isLoggedIn = (req,res,next) => {
--- routes/listing.js
+++ routes/listing.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-// const ExpressError = require("../utils/expressError.js");
+// const ExpressError = require("../utils/ExpressError.js");
 // const { listingSchema } = require("../schema.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");  //we use this to check whether the user is logged in  or not
